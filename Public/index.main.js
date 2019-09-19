3fac0016ccd13576556b3d24f17201cdd30e222c
Aziz Qureshi <e3017@pha.pacificsource.com>
2019-09-19 15:39:44 -0700

    fixed the ejs include external js file.

---
+++ Public/index.main.js
@@ -0,0 +1,1 @@
+console.log("I am in the main js file")
--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const chalk = require('chalk');
-const mongoose = require('mongoose');
-const id = require('./public/js/utility/randnum')
-
-var Subjects = require('./public/js/subjects/subject.controller-old');
-
-var dbName = 'curriculum';
+const path = require('path')
 var port = require('./Public/js/DB/properties').PORT;
 var db = require('./Public/js/DB/database');
-var subjectSchema = require('./public/js/subjects/subject.schemas');
 var controller = require('./public/js/subjects/subject.controller')
-var subjectColl = 'subjects';
 
+var dbName = 'curriculum';
 var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true})) ;
 
-app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname + '/public')));
+
+
 
 
 
