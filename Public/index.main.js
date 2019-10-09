18022fd818fc2f8fcf75545f975b2bdf7ca2fc6a
Aziz Qureshi <e3017@pha.pacificsource.com>
2019-10-09 11:00:23 -0700

    Committing changes

--- Public/index.main.js
+++ Public/index.main.js
@@ -8,11 +8,25 @@ function RunMain(){
     var t = document.getElementById('calendar');
 
     var c = new FullCalendar.Calendar(t, {
-        plugins: [ 'dayGrid']
+        plugins: [ 'dayGrid'],
+        events: [
+            {
+                title: 'The title !',
+                start: '2019-10-01',
+            }
+        ]
     });
-
     c.render();
 
+    dayClicked();
+
+    $(".fc-button").click(dayClicked);
 }
 
+function dayClicked(){
+    $(".fc-day").click(doWhenDayClicked); 
+}
 
+function doWhenDayClicked(){
+    alert("Handler clicked for the day element.");
+}
